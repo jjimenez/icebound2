6c169542d24fd892880d193f02bdd5df030cd010
Jose Jimenez <to.jose.jimenez@gmail.com>
2012-11-16 11:04:11 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icebound2::Application.config.secret_token = '7b0548ef429969491742de6775051406c26acd6f38447c321282c35f3d3d52061c0ad7ea9431ce8129ef84c8293be0e4aea3234bbe2db1aa918ceeac23aee90b'
